5a884dfa622565e63c85b685ef85a614f4872c86
Damian Dziaduch <damian@dziaduch.pl>
2019-09-26 05:54:15 +0200

    Fix example

--- backward incompatible/get_declared_classes() no longer returns anonymous classes that havent been instantiated yet.php
+++ backward incompatible/get_declared_classes() no longer returns anonymous classes that havent been instantiated yet.php
@@ -1,10 +1,8 @@
 <?php declare(strict_types=1);
 
-class Base {}
-
 class Factory {
-    public static function build(): Base {
-        return new class() extends Base {};
+    public static function build(): object {
+        return new class() {};
     }
 }
 
@@ -13,7 +11,7 @@ function isUnusedAnonymousClassDeclared(): bool {
         $reflectionClass = new ReflectionClass($class);
         if (
             $reflectionClass->isAnonymous()
-            && $reflectionClass->isSubclassOf(Base::class)
+            && $reflectionClass->getFileName() === __FILE__
         ) {
             return true;
         }
@@ -24,9 +22,8 @@ function isUnusedAnonymousClassDeclared(): bool {
 // 7.3 -> true
 // 7.4 -> false
 var_dump(isUnusedAnonymousClassDeclared());
+
 Factory::build();
 // 7.3 -> true
 // 7.4 -> true
 var_dump(isUnusedAnonymousClassDeclared());
-
-
