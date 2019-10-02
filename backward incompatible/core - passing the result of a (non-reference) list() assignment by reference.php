621ba2e18a25c2131ab1686d71b655dbea37d02a
Damian Dziaduch <damian@dziaduch.pl>
2019-10-02 05:45:20 +0200

    Add new example

---
+++ backward incompatible/core - passing the result of a (non-reference) list() assignment by reference.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+function test1() {
+    $a = [1, 2];
+    $c =& $a;
+    list($c, $b) = $a;
+    var_dump($a, $b);
+}
+function test2() {
+    $a = [1, 2];
+    $_a = "a";
+    list($$_a, $b) = $a;
+    var_dump($a, $b);
+}
+test1();
+test2();
+
