d9242d5e80f0552a0a27d42174796970b621a02b
Damian Dziaduch <damian@dziaduch.pl>
2019-10-03 05:08:56 +0200

    Improve readability in example

--- backward incompatible/core/passing the result of a (non-reference) list() assignment by reference.php
+++ backward incompatible/core/passing the result of a (non-reference) list() assignment by reference.php
@@ -6,12 +6,19 @@ function test1() {
     list($c, $b) = $a;
     var_dump($a, $b);
 }
+
 function test2() {
     $a = [1, 2];
     $_a = "a";
     list($$_a, $b) = $a;
     var_dump($a, $b);
 }
+
+// 7.3 -> 1, NULL
+// 7.4 -> 1, 2
 test1();
+
+// 7.3 -> 1, NULL
+// 7.4 -> 1, 2
 test2();
 
