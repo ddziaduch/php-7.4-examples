abda44d8a28c1a30ea5ebb5d5919aded9e32e8b7
Damian Dziaduch <damian@dziaduch.pl>
2019-11-12 05:41:12 +0100

    Add missing error reporting

--- backward_incompatible/curl/deprecate_and_ignore_version_parameter_of_curl_version.php
+++ backward_incompatible/curl/deprecate_and_ignore_version_parameter_of_curl_version.php
@@ -1,5 +1,9 @@
 <?php declare(strict_types=1);
 
+error_reporting(E_ALL);
+
+// 7.3: brak ostrzeżen
+// 7.4: Deprecated: curl_version(): the $version parameter is deprecated
 curl_version(CURLVERSION_NOW);
 // 7.3: brak ostrzeżen
 // 7.3: Warning: curl_version(): $version argument ignored
