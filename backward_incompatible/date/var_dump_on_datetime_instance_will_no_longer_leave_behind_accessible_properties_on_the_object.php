87dcbddb0a06310ae28eec2ca340f3d4d513e568
Damian Dziaduch <damian@dziaduch.pl>
2019-12-02 06:05:20 +0100

    Add comments to example

--- backward_incompatible/date/var_dump_on_datetime_instance_will_no_longer_leave_behind_accessible_properties_on_the_object.php
+++ backward_incompatible/date/var_dump_on_datetime_instance_will_no_longer_leave_behind_accessible_properties_on_the_object.php
@@ -3,9 +3,13 @@
 error_reporting(E_ALL);
 
 $d1 = DateTime::createFromFormat("Ymd\THis\Z", '20170920T091600Z');
+// 7.3: Notice: Undefined property: DateTime::$date
+// 7.4: Notice: Undefined property: DateTime::$date
 echo $d1->date, "\n";
 
 $d2 = DateTime::createFromFormat("Ymd\THis\Z", '20170920T091600Z');
+// 7.3: 2017-09-20 09:16:00.000000
+// 7.4: Notice: Undefined property: DateTime::$date
 print_r($d2);
 echo $d2->date, "\n";
 
